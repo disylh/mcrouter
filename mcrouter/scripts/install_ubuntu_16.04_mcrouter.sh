30102aa195c11446d0eff48cae6b917118ee810a
lukeye <lukeye@lukeye-mbp.dhcp.thefacebook.com>
2019-10-24 15:21:17 -0700

    split travis-ci build to two jobs to avoid timeout

---
+++ mcrouter/scripts/install_ubuntu_16.04_mcrouter.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+# Copyright (c) Facebook, Inc. and its affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+set -ex
+
+[ -n "$1" ] || ( echo "Install dir missing"; exit 1 )
+
+cd "$(dirname "$0")" || ( echo "cd fail"; exit 1 )
+
+./get_and_build_by_make.sh "Makefile_ubuntu-16.04" "mcrouter" "$@"
